0a7028634408ff272456f858f5468918b1d6c546
Gerald Post <53874830+post-g@users.noreply.github.com>
2020-12-09 09:15:04 -0800

    Update sticky_note_text.py
    Adding additional comments

--- sticky_note_text.py
+++ sticky_note_text.py
@@ -20,18 +20,21 @@ def get_events(text: list) -> dict:
 
     return events
 
-
+# StickyNote class that creates new object to create connection and
+# interact with the database that hosts the sticky note information 
 class StickyNote:
     def __init__(self, database):
         self._database = database
         self._conn = None
-
+    
     def get_database(self):
         return self._database
 
     def get_conn(self):
         return self._conn
-
+    
+    # Creating a connection with the sqllite database and storing connection in 
+    # database member variable 
     def create_connection(self, sticky_db):
         try:
             conn = sqlite3.connect(sticky_db)
@@ -39,15 +42,18 @@ class StickyNote:
             print(erorr)
 
         self._conn = conn
-
+    
+    # After connection is made, using SELECT statment to access 
+    # the desired rows and columns from the db 
     def get_sticky_note_text(self):
         cur = self._conn.cursor()
         cur.execute("SELECT Text FROM Note")
         rows = cur.fetchone()
         return rows
 
-
+# Main function that creates a StickyNote object and gets a list of the desired text 
 def create_sticky():
+    # replace with own user name for the path to the sqllite database 
     database = r"C:\Users\Gerald Post\AppData\Local\Packages\Microsoft.MicrosoftStickyNotes_8wekyb3d8bbwe\LocalState\plum.sqlite"
 
     sticky_note = StickyNote(database)
@@ -55,6 +61,7 @@ def create_sticky():
     sticky_note.create_connection(database)
     with sticky_note.get_conn():
         data = sticky_note.get_sticky_note_text()
+        # using RE to remove unwanted ID text attached to the strings 
         text = (re.sub('(\\n)?(\\\\id=)(\d|\w){8}-(\d|\w){4}-(\d|\w){4}-(\d|\w){4}-(\d|\w){12}\s', '~', data[0]).split(
             '~'))
         events = get_events(text)
